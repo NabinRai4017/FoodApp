3db65f27c893ad739d92719c28aa176e694246d2
NAbin RAi <nabinr@gmeremit.com>
2023-04-29 17:00:32 +0545

    image changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:food_app/component/app_life_cycle.dart';
 import 'package:food_app/component/screen/home_screen.dart';
